6771dc85a81b36ca347fb4338e762368fce5cfe2
Yens <yens19930401@gmail.com>
2023-04-02 22:58:48 -0500

    backend

--- src/main/java/api/java/backend/controller/v1/ExampleController.java
+++ src/main/java/api/java/backend/controller/v1/ExampleController.java
@@ -1,4 +1,4 @@
-package api.java.backend.controller;
+package api.java.backend.controller.v1;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
